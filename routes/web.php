554ec74257d91c64ec7ad6ece92363c28fd77f7f
yaboijem <bantoljeremy27@gmail.com>
2026-08-01 22:20:25 +0800

    chore: remove dev test page, gate dev OTP route behind APP_DEBUG

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+if (config('app.debug')) {
+    Route::get('/dev/otp/{employeeId}', function (string $employeeId) {
+        $secret = App\Models\User::where('employee_id', $employeeId)->value('two_factor_secret');
+
+        if (! $secret) {
+            return response()->json(['code' => null, 'message' => 'No TOTP secret configured for this account.'], 404);
+        }
+
+        return response()->json(['code' => (new PragmaRX\Google2FA\Google2FA)->getCurrentOtp($secret)]);
+    });
+}
